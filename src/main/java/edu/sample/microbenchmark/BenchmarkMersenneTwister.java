46dcc4d41b03c0b160858243a4cd3462266bc6a4
Tần Quảng <20223064+tumatanquang@users.noreply.github.com>
2024-07-31 18:56:51 +0700

    Add files via upload

--- src/main/java/edu/sample/microbenchmark/BenchmarkMersenneTwister.java
+++ src/main/java/edu/sample/microbenchmark/BenchmarkMersenneTwister.java
@@ -1,12 +1,13 @@
 package edu.sample.microbenchmark;
 import java.util.concurrent.TimeUnit;
-import javax.xml.ws.handler.MessageContext.Scope;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
