239ef35e4238bcc2095a972e3f2c359eefa8e77e
Tần Quảng <20223064+tumatanquang@users.noreply.github.com>
2026-01-01 01:48:32 +0700

    Update dependencies

--- src/main/java/edu/sample/microbenchmark/BenchmarkMultiThreadjOSClock.java
+++ src/main/java/edu/sample/microbenchmark/BenchmarkMultiThreadjOSClock.java
@@ -6,6 +6,7 @@ import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OperationsPerInvocation;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.Scope;
@@ -24,7 +25,7 @@ import uc.j.OSClock;
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.SECONDS)
 public class BenchmarkMultiThreadjOSClock {
-	private static final int BATCH_SIZE = 10_000_000;
+	private static final int BATCH_SIZE = 1_000_000;
 	private static final java.time.Clock CLOCK = java.time.Clock.systemUTC();
 	@Setup(Level.Trial)
 	public void warmJNI() {
@@ -38,54 +39,63 @@ public class BenchmarkMultiThreadjOSClock {
 	@Benchmark
 	@Warmup(iterations = 5, batchSize = BATCH_SIZE)
 	@Measurement(iterations = 10, batchSize = BATCH_SIZE)
+	@OperationsPerInvocation(BATCH_SIZE)
 	public void SystemTimeMillis(Blackhole bh) {
 		bh.consume(System.currentTimeMillis());
 	}
 	@Benchmark
 	@Warmup(iterations = 5, batchSize = BATCH_SIZE)
 	@Measurement(iterations = 10, batchSize = BATCH_SIZE)
+	@OperationsPerInvocation(BATCH_SIZE)
 	public void OSClockTimeMills(Blackhole bh) {
 		bh.consume(OSClock.currentTimeMillis());
 	}
 	@Benchmark
 	@Warmup(iterations = 5, batchSize = BATCH_SIZE)
 	@Measurement(iterations = 10, batchSize = BATCH_SIZE)
+	@OperationsPerInvocation(BATCH_SIZE)
 	public void SystemNanoTime(Blackhole bh) {
 		bh.consume(System.nanoTime());
 	}
 	@Benchmark
 	@Warmup(iterations = 5, batchSize = BATCH_SIZE)
 	@Measurement(iterations = 10, batchSize = BATCH_SIZE)
+	@OperationsPerInvocation(BATCH_SIZE)
 	public void OSClockNanoTime(Blackhole bh) {
 		bh.consume(OSClock.nanoTime());
 	}
 	@Benchmark
 	@Warmup(iterations = 5, batchSize = BATCH_SIZE)
 	@Measurement(iterations = 10, batchSize = BATCH_SIZE)
+	@OperationsPerInvocation(BATCH_SIZE)
 	public void OSClockMonotonicNanos(Blackhole bh) {
 		bh.consume(OSClock.monotonicNanos());
 	}
 	@Benchmark
 	@Warmup(iterations = 5, batchSize = BATCH_SIZE)
 	@Measurement(iterations = 10, batchSize = BATCH_SIZE)
+	@OperationsPerInvocation(BATCH_SIZE)
 	public void OSClockEpochMillis(Blackhole bh) {
 		bh.consume(OSClock.epochMillis());
 	}
 	@Benchmark
 	@Warmup(iterations = 5, batchSize = BATCH_SIZE)
 	@Measurement(iterations = 10, batchSize = BATCH_SIZE)
+	@OperationsPerInvocation(BATCH_SIZE)
 	public void JavaTimeMillis(Blackhole bh) {
 		bh.consume(CLOCK.millis());
 	}
 	@Benchmark
 	@Warmup(iterations = 5, batchSize = BATCH_SIZE)
 	@Measurement(iterations = 10, batchSize = BATCH_SIZE)
+	@OperationsPerInvocation(BATCH_SIZE)
 	public void SystemSecondDivide(Blackhole bh) {
 		bh.consume(System.currentTimeMillis() / 1000L);
 	}
 	@Benchmark
 	@Warmup(iterations = 5, batchSize = BATCH_SIZE)
 	@Measurement(iterations = 10, batchSize = BATCH_SIZE)
+	@OperationsPerInvocation(BATCH_SIZE)
 	public void OSClockEpochSecond(Blackhole bh) {
 		bh.consume(OSClock.epochSeconds());
 	}
